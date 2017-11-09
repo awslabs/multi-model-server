77a3a5a535f5c66e134708430c6e2e8f0df17b3a
Hagay Lupesko <lupesko@gmail.com>
2017-11-08 23:24:34 -0800

    updated readme to reflect current CLI name and new version for the PIP package

--- setup.py
+++ setup.py
@@ -15,9 +15,9 @@ pkgs.append('tools')
 
 setup(
     name='deep-model-server',
-    version='0.1.1',
-    description='Deep Model Server is a tool for deploying neural net models for inference',
-    url='https://github.com/deep-learning-tools/deep-model-server',
+    version='0.1.2',
+    description='Portico is a tool for packaging neural net models and deploying them for inference',
+    url='https://github.com/awslabs/portico',
     keywords='MXNet Model Serving Deep Learning Inference',
     packages=pkgs,
     install_requires=['mxnet>=0.11.0', 'Flask', 'Pillow', 'requests', 'flask-cors', 'psutil'],

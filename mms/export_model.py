3da4880639acdb204f03451afb1ea7e2339f427e
Naveen Swamy <mn.naveen@gmail.com>
2017-11-29 13:11:40 -0800

    update exporter to create manifest, service_file, etc

--- mms/export_model.py
+++ mms/export_model.py
@@ -7,16 +7,22 @@
 # on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
 # express or implied. See the License for the specific language governing
 # permissions and limitations under the License.
+from boto.services import service
+from docutils.nodes import paragraph
 
 '''Command line interface to export model files to be used for inference by MXNet Model Server
 '''
 
 import os
+import glob
 import logging
 import json
 import zipfile
+import imp
 import mxnet as mx
 from mms.arg_parser import ArgParser
+import mms.model_service.mxnet_model_service as base_service 
+import mms.model_service.mxnet_vision_service as vision_service
 
 try:
     basestring
@@ -26,14 +32,16 @@ except NameError:
 
 SIG_REQ_ENTRY = ['inputs', 'input_type', 'outputs', 'output_types']
 VALID_MIME_TYPE = ['image/jpeg', 'application/json']
+MANIFEST_SCHEMA = 'manifest-schema.json'
+SIGNATURE_FILE = 'signature.json'
 
-def _check_signature(model_path):
+def validate_signature(model_path):
     '''Internal helper to check signature error when exporting model with CLI.
     '''
-    sig_file = '%s/signature.json' % (model_path)
-    assert os.path.isfile(sig_file), \
+    sig_file = '%s/%s' % (SIGNATURE_FILE, model_path)
+    assert os.path.isfile(signature_file), \
         "signature.json is not found in %s." % (model_path)
-    with open(sig_file) as js_file:
+    with open(signature_file) as js_file:
         signature = json.load(js_file)
 
     assert 'input_type' in signature and 'output_type' in signature, \
@@ -65,165 +73,154 @@ def _check_signature(model_path):
         assert 'data_shape' in output, 'data_shape is required for output.'
         assert isinstance(output['data_shape'], list), 'data_shape value must be list.'
 
+    return signature_file
 
-def _export_model(args):
+def validate_symbol(model_path):
+    '''
+    Checks if a single Symbol file ending with '-symbol.json' file exists within the model_path
+    '''
+    symbol_file_postfix = '-symbol.json'
+    
+    sym_files = glob.glob(model_path + symbol_file_postfix)
+    
+    assert(len(sym_files) == 1, \
+           "should have 1 symbol file ending with -symbol.json in the filename, given:%s in model_path:%s" \
+           %str(sym_files), model_path)
+    
+    return sym_files[0]
+
+def validate_params(params_file):
+    '''
+    Checks if a single parameter file ending with .params extension in the model_path
+    and returns that file
+    '''
+    params_file_postfix = '.params'
+    
+    param_files = glob.glob(model_path + symbol_file_postfix)
+    
+    assert(len(param_files) == 1, \
+           "should have 1 parameter file ending with .params in the filename, given:%s in model_path:%s" \
+           %str(param_files), model_path)
+    
+    return param_files[0]
+
+def validate_service(model_path, service_file, signature_file):    
+    
+    if not service_file:
+
+        assert (os.path.isfile(service_file) or (len(glob.glob(model_path + service_file)) == 1)), \
+            "Service File not found in %s or in %s." % (service_file, model_path)
+            
+        service_file = service_file if os.path.isfile(service_file) \
+            else glob.glob(model_path + service_file)[0]
+        
+        module = None
+        try:
+            module =  imp.load_source(
+                os.path.splitext(os.path.basename(service_file))[0],
+                service_file)
+        except Exception as e:
+            raise Exception('Incorrect or missing service file: ' + service_file)
+    
+        classes = [cls[1] for cls in inspect.getmembers(module, inspect.isclass)]
+        # Check if subclass of MXNetBaseService
+        service_classes = list(filter(lambda cls: issubclass(cls, MXNetBaseService), classes))
+        
+        assert (len(service_classes) == 1, \
+                "There should be 1 Service class derived from MXNetBaseService, found %s classes" % str(service_classes))
+    
+    else:
+        input_type = json.load(signature_file)['input_type']
+        
+        if input_type == 'image/jpeg':
+            service_file = vision_service.__file__
+        elif input_type == 'application/json':
+            service_file = base_service.__file__
+        else:
+            assert(input_type in VALID_MIME_TYPE, \
+                   "input_type should be one of %s or have your own service file handling it" % str(VALID_MIME_TYPE))
+
+        #if service_file is a compile file, get the corresponding python code file
+        if service_file.endswith('.pyc'):
+            service_file = service_file[:-1]            
+            assert(os.path.isfile(service_file), \
+                   "Vision Service File is missing in mms installation")
+        
+    return service_file
+    
+def generate_manifest(symbol_file, params_file, service_file, signature_file, model_name):
+    manifest = {}
+    manifest["Model-Archive-Version"] = 0.2
+    manifest["Model-Archive-Description"] = model_name
+    manifest["License"] = "Apache 2.0"
+    manifest["Created-By"] = {"Model-Server": 0.2}
+    
+    manifest["Model"] = {}
+    manifest["Model"]["Symbol"] = os.path.split(symbol_file)[1]
+    manifest["Model"]["Parameters"] = os.path.split(params_file)[1]
+    manifest["Model"]["Signature"] = os.path.split(signature_file)[1]
+    manifest["Model"]["Service"] =  os.path.split(service_file)[1]
+    manifest["Model"]["Description"] = model_name
+    manifest["Model"]["Model-Format"] = "MXNet-Symbolic"
+    manifest["Engine"]  = {"MXNet":0.12}
+    
+    return manifest
+    
+def export_model(model_name, model_path, service_file=None):
     '''Internal helper for the exporting model command line interface.
     '''
-    model_name = args.model_name
-    model_path = args.model_path
     destination = os.getcwd()
+    
     if model_path.startswith('~'):
         model_path = os.path.expanduser(model_path)
-    _check_signature(model_path)
+    
+    model_path = os.path.join(model_path, os.sep) if model_path[-1] != '/' else model_path
+    
+    signature_file = validate_signature(model_path)
+    
+    service_file = validate_service(model_path, service_file, signature_file)
 
-    symbol_file_postfix = '-symbol.json'
-    symbol_file_num = 0
-    file_list = []
+    symbol_file = validate_symbol(model_path)
+    
+    params_file = validate_params(model_path)
+    
+    manifest = generate_manifest(symbol_file, params_file, service_file, signature_file, model_name)
+    manifest_file = os.path.join(model_path, "manifest.json")
+    with open(manifest_file, 'w') as m:
+        json.dump(manifest, m)
+
+    #manifest schema
+    import mms
+    mms_pkg_loc = mms.__file__.split('mms')[0]
+    manifest_schema_file = os.path.join(mms_pkg_loc, MANIFEST_SCHEMA)
+    assert(os.path.isfile(manifest_schema_file), \
+           "manifest-schema file missing mms pkg location:%s" % mms_pkg_loc)
+
+    file_list = [signature_file, service_file, symbol_file, params_file, manifest_file, manifest_schema_file]
+    
+    #add all the auxillary files    
     for dirpath, _, filenames in os.walk(model_path):
         for file_name in filenames:
-            if file_name.endswith(symbol_file_postfix):
-                symbol_file_num += 1
-            file_list.append(os.path.join(dirpath, file_name))
-    if symbol_file_num == 0:
-        logging.warning("No MXNet model symbol json file is found. "
-                        "You may need to manually load the model in your service class.")
-    if symbol_file_num > 1:
-        logging.warning("More than one model symbol json file was found. "
-                        "You must manually load the model in your service class.")
+            file_name = os.path.join(model_path, file_name)
+            
+            if os.path.isfile(file_name) and filename not in file_list:
+                file_list.append(file_name)
 
     export_file = os.path.join(destination,'%s.model' % model_name)
-    if os.path.isfile(export_file):
-        raise RuntimeError("%s.model already exists in %s directory." % (model_name, destination))
+
+    assert(os.path.isfile(export_file), \
+           "%s.model already exists in %s directory." % (model_name, destination))
+
     with zipfile.ZipFile(export_file, 'w') as zip_file:
         for item in file_list:
             zip_file.write(item, os.path.basename(item))
-    print('Successfully exported %s model. Model file is located in %s directory.'
+
+    logging.info('Successfully exported %s model. Model file is located in %s directory.'
           % (model_name, destination))
-
-
-def export_serving(model, filename, signature, export_path=None, aux_files=None):
-    """Export a module object to a .model file to be used by MXNet Model Server.
-
-    Parameters
-    ----------
-    model : mx.mod.Module or mx.mod.BucketModule
-        Module object to be exported.
-    filename : str
-        Prefix of exported model file.
-    signature : dict
-        A dictionary containing model input and output information.
-        Data names or data shapes of inputs and outputs can be automatically
-        collected from the module. They are optional. You need to specify the
-        MIME type of the inputs and outputs for the http request. Currently only
-        'image/jpeg' and 'application/json' are supported. All inputs should
-        have the same type. This also applies for outputs.
-        An example signature would be:
-            signature = { "input_type": "image/jpeg", "output_type": "application/json" }
-        A full signature containing inputs and outputs:
-            {
-                "input_type": "image/jpeg",
-                "inputs" : [
-                    {
-                        "data_name": "data",
-                        "data_shape": [1, 3, 224, 224]
-                    }
-                ],
-                "outputs" : [
-                    {
-                        "data_name": "softmax",
-                        "data_shape": [1, 1000]
-                    }
-                ],
-                "output_type": "application/json"
-            }
-    export_path : str
-        Destination path for export file. By default the model file
-        is saved to current working directory.
-    aux_files : List
-        A list of string containing other utility files for inference.
-        One example is class label file for classification task.
-
-    Examples
-    --------
-    >>> model1 = mx.mod.Module(...)
-    >>> signature1 = { "input_type": "image/jpeg", "output_type": "application/json" }
-    >>> export_serving(model1, filename='resnet-18', signature=signature1,
-    >>>                aux_files=['synset.txt'])
-    >>>
-    >>> model2 = mx.mod.Module(...)
-    >>> signature2 = {
-    >>>                  "input_type": "image/jpeg",
-    >>>                  "inputs" : [
-    >>>                      {
-    >>>                          "data_name": "data",
-    >>>                          "data_shape": [1, 3, 224, 224]
-    >>>                      }
-    >>>                  ],
-    >>>                  "outputs" : [
-    >>>                      {
-    >>>                          "data_name": "softmax",
-    >>>                          "data_shape": [1, 1000]
-    >>>                      }
-    >>>                  ]
-    >>>                  "output_type": "application/json"
-    >>>              }
-    >>> export_serving(model2, filename='resnet-152', signature=signature2,
-    >>>                aux_files=['synset.txt'])
-    Exported model to "resnet-18.model"
-    Exported model to "resnet-152.model"
-    """
-    assert issubclass(type(model), mx.mod.BaseModule) or issubclass(type(model), mx.gluon.Block), \
-        "Model is type %s. It must be a subclass of mx.mod.BaseModule or mx.gluon.BLock." % (type(model))
-
-    epoch_placeholder = 0
-    destination = export_path or os.getcwd()
-    if destination.startswith('~'):
-        destination = os.path.expanduser(destination)
-    sig_file = '%s/signature.json' % (destination)
-
-    if issubclass(type(model), mx.mod.BaseModule):
-        if 'inputs' not in signature:
-            signature['inputs'] = list()
-            for name, shape in model.data_shapes:
-                signature['inputs'].append({
-                    'data_name': name,
-                    'data_shape': list(shape)
-                })
-        if 'outputs' not in signature:
-            signature['outputs'] = list()
-            for name, shape in model.output_shapes:
-                signature['outputs'].append({
-                    'data_name': name,
-                    'data_shape': list(shape)
-                })
-        model.save_checkpoint('%s/%s' % (destination, filename), epoch_placeholder)
-    else:
-        assert 'inputs' in signature and 'outputs' in signature, \
-            "Inputs and outputs information is required for gluon model signature."
-        model.export('%s/%s' % (destination, filename))
-
-    with open(sig_file, 'w') as sig:
-        json.dump(signature, sig)
-    _check_signature(destination)
-
-    file_list = ['%s/%s-symbol.json' % (destination, filename), '%s/%s-%04d.params' %
-                    (destination, filename, epoch_placeholder), sig_file]
-    if aux_files:
-        file_list += aux_files
-
-    abs_model_path = os.path.join(destination,'%s.model' % filename)
-    if os.path.isfile(abs_model_path):
-        raise RuntimeError("%s already exists." % (abs_model_path))
-    with zipfile.ZipFile(abs_model_path, 'w') as zip_file:
-        for item in file_list:
-            zip_file.write(item)
-            os.remove(item)
-    print('Exported model to %s/%s.model' %( destination, filename))
-
 
 def export():
     args = ArgParser.export_parser().parse_args()
-    _export_model(args)
+    export_model(args.model_name, args.model_path, args.service_file_path)
 
 if __name__ =='__main__':
     export()

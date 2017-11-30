b3f6964bb1943893331e149c03df3c5de86b327a
Ruofei Yu <yuruofeifei@gmail.com>
2017-11-30 11:17:43 -0800

    Fix

--- mms/tests/integration_tests/extend_export_predict_service_test.py
+++ mms/tests/integration_tests/extend_export_predict_service_test.py
@@ -79,7 +79,7 @@ def test_ssd_extend_export_predict_service(tmpdir):
     start_test_server_thread = Thread(target = setup_ssd_server, args=(str(tmpdir),))
     start_test_server_thread.daemon = True
     start_test_server_thread.start()
-    time.sleep(90)
+    time.sleep(15)
     output = subprocess.check_output('curl -X POST http://127.0.0.1:8080/SSD/predict -F "data=@{}/street.jpg"'.format(str(tmpdir)), shell=True)
     if sys.version_info[0] >= 3:
         output = output.decode("utf-8")

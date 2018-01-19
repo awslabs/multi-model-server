bf2f7b4010968031c1176ebf46ec969a244e0a3f
Jonathan Esterhazy <esterhaz@amazon.com>
2018-01-19 06:28:30 -0800

    update error messages for model export

--- mms/export_model.py
+++ mms/export_model.py
@@ -11,18 +11,21 @@
 '''Command line interface to export model files to be used for inference by MXNet Model Server
 '''
 
-import os
 import glob
-import json
-import zipfile
-import mxnet as mx
 import inspect
+import json
+import os
+import re
+import shutil
+import zipfile
+
+import mxnet as mx
+
+import mms
 from mms.arg_parser import ArgParser
-import mms.model_service.mxnet_model_service as base_service 
-import mms.model_service.mxnet_vision_service as vision_service
-from mms.model_service.mxnet_model_service import MXNetBaseService
-from mms.model_service.model_service import load_service
 from mms.log import get_logger
+from mms.model_service.model_service import load_service
+from mms.model_service.mxnet_model_service import MXNetBaseService
 
 logger = get_logger()
 
@@ -31,6 +34,10 @@ try:
 except NameError:
     basestring = str
 
+MMS_SERVICE_FILES = {k: os.path.splitext(v.__file__)[0] + '.py' for k, v in {
+    'image/jpeg': mms.model_service.mxnet_vision_service,
+    'application/json': mms.model_service.mxnet_model_service
+}.items()}
 
 SIG_REQ_ENTRY = ['inputs', 'input_type', 'outputs', 'output_types']
 VALID_MIME_TYPE = ['image/jpeg', 'application/json']
@@ -41,13 +48,58 @@ MANIFEST_FILE_NAME = 'MANIFEST.json'
 MXNET_TYPE = 'mxnet'
 ONNX_TYPE = 'onnx'
 
+NO_MODEL_FILES_MESSAGE = '''
+No model files found in the model directory {}.
+
+mxnet-model-export supports the exporting of MXNet and ONNX models to a 
+mxnet-model-server .model file. 
+
+MXNet models are expected as two files in the same directory, a params file
+and a symbol file, both with the same prefix and where 0000 is the param's 
+epoch number (any number from 0 to n). 
+Example: modelname-0000.params and modelname-symbol.json.
+ 
+ONNX models are expected as one file. 
+Example: modelname.onnx. 
+
+See https://github.com/onnx/onnx for converting PyTorch, Caffe2, CNTK, 
+and other models to the ONNX format.
+'''.strip()
+
+MIXED_MODEL_FILES_MESSAGE = 'More than one model type is present in the model directory {}.'
+INCOMPLETE_MODULE_FILES_MESSAGE = '''
+Incomplete MXNet model found in the model directory {}.
+
+MXNet models require a parameter file and a symbol file.
+Example: modelname-0000.params and modelname-symbol.json.
+'''.strip()
+
+NO_EPOCH_NUMBER_MESSAGE = '''
+No epoch number found in the parameter filename {}. 
+
+When exporting an MXNet model, mxnet-model-export expects a parameters file
+that includes the epoch number in the filename. 0000 is usually sufficient, 
+but if known, you can supply any epoch number in the format: modelname-1234.params.'
+'''.strip()
+
+MODEL_PREFIX_MISMATCH_MESSAGE = '''
+Your parameters file and symbols file naming prefix do not match. 
+
+When exporting an MXNet model, mxnet-model-export expects two files in the 
+same directory, a params file and a symbol file, both with the same prefix 
+(a common name) and where 0000 is the param's epoch number (any number from 0 to n). 
+Example: modelname-0000.params and modelname-symbol.json.
+'''.strip()
+
+
 def validate_signature(model_path):
-    '''Internal helper to check signature error when exporting model with CLI.
-    '''
+    """
+    Internal helper to check signature error when exporting model with CLI.
+    """
     signature_file = os.path.join(model_path, SIGNATURE_FILE)
 
     assert os.path.isfile(signature_file), \
-        "signature.json is not found in %s." % (model_path)
+        "signature.json is not found in %s." % model_path
     with open(signature_file) as js_file:
         signature = json.load(js_file)
 
@@ -82,52 +134,13 @@ def validate_signature(model_path):
 
     return signature_file
 
-def validate_onnx(model_path):
-    '''
-    Checks if a single onnx file ending with '*.onnx' file exists within the model_path
-    '''
-    symbol_file_postfix = '.onnx'
-
-    onnx_files = glob.glob(model_path + "/*" + symbol_file_postfix)
-
-    if len(onnx_files) == 1:
-        return onnx_files[0]
-    return None
-
-def validate_symbol(model_path):
-    '''
-    Checks if a single Symbol file ending with '-symbol.json' file exists within the model_path
-    '''
-    symbol_file_postfix = '-symbol.json'
-    sym_files = glob.glob(model_path + "*" + symbol_file_postfix)
-    
-    assert len(sym_files) == 1, \
-           "should have 1 symbol file ending with -symbol.json in the filename, given:%s in model_path:%s" \
-           % (str(sym_files), model_path)
-    
-    return sym_files[0]
-
-def validate_params(model_path):
-    '''
-    Checks if a single parameter file ending with .params extension in the model_path
-    and returns that file
-    '''
-    params_file_postfix = '.params'
-    
-    param_files = glob.glob(model_path + "*" + params_file_postfix)
-    
-    assert len(param_files) == 1, \
-           "should have 1 parameter file ending with .params , given:%s in model_path:%s" \
-           %(str(param_files), model_path)
-    
-    return param_files[0]
 
 def validate_service(model_path, service_file, signature_file):
     if service_file:
 
         assert os.path.isfile(service_file) or os.path.isfile(os.path.join(model_path, service_file)), \
             "Service File not found in %s or in %s." % (service_file, model_path)
-            
+
         service_file = service_file if os.path.isfile(service_file) \
             else glob.glob(model_path + service_file)[0]
 
@@ -136,118 +149,168 @@ def validate_service(model_path, service_file, signature_file):
         classes = [cls[1] for cls in inspect.getmembers(module, inspect.isclass)]
         # Check if subclass of MXNetBaseService
         service_classes = list(filter(lambda cls: issubclass(cls, MXNetBaseService), classes))
-        
+
         assert len(service_classes) > 1, \
-                "The Service class should be derived from MXNetBaseService, found %s classes" % str(service_classes)
-        
-        #remove the compiled python code
+            "The Service class should be derived from MXNetBaseService, found %s classes" % str(service_classes)
+
+        # remove the compiled python code
         if os.path.exists(service_file + 'c'):
             os.remove(service_file + 'c')
-    
+
     else:
         input_type = None
         with open(signature_file) as js_file:
             input_type = json.load(js_file)['input_type']
-        
-        if input_type == 'image/jpeg':
-            service_file = vision_service.__file__
-        elif input_type == 'application/json':
-            service_file = base_service.__file__
-        else:
-            assert input_type in VALID_MIME_TYPE, \
-                   "input_type should be one of %s or have your own service file handling it" % str(VALID_MIME_TYPE)
 
-        #if service_file is a compile file, get the corresponding python code file
-        if service_file.endswith('.pyc'):
-            service_file = service_file[:-1]            
-            assert os.path.isfile(service_file), \
-                   "Vision Service File is missing in mms installation" 
-        
+        if input_type not in VALID_MIME_TYPE:
+            raise ValueError("input_type should be one of %s or have your own service file handling it"
+                             % str(VALID_MIME_TYPE))
+
+        service_file = MMS_SERVICE_FILES[input_type]
+        if not os.path.exists(service_file):
+            raise ValueError('Service File {} is missing in mms installation'
+                             .format(os.path.basename(service_file)))
+
     return service_file
-    
+
+
 def generate_manifest(symbol_file, params_file, service_file, signature_file, model_name):
     manifest = {}
     manifest["Model-Archive-Version"] = MODEL_ARCHIVE_VERSION
     manifest["Model-Archive-Description"] = model_name
     manifest["License"] = "Apache 2.0"
     manifest["Model-Server"] = MODEL_SERVER_VERSION
-    
+
     manifest["Model"] = {}
     manifest["Model"]["Symbol"] = os.path.split(symbol_file)[1]
     manifest["Model"]["Parameters"] = os.path.split(params_file)[1]
     manifest["Model"]["Signature"] = os.path.split(signature_file)[1]
-    manifest["Model"]["Service"] =  os.path.split(service_file)[1]
+    manifest["Model"]["Service"] = os.path.split(service_file)[1]
     manifest["Model"]["Description"] = model_name
     manifest["Model"]["Model-Name"] = model_name
     manifest["Model"]["Model-Format"] = "MXNet-Symbolic"
-    manifest["Engine"]  = {"MXNet":0.12}
-    
+    manifest["Engine"] = {"MXNet": 0.12}
+
     return manifest
-    
 
-def convert_to_mxnet(model_path):
-    onnx_file = validate_onnx(model_path)
 
+def convert_onnx_model(model_path, onnx_file):
+    import onnx_mxnet
+    model_name = os.path.splitext(os.path.basename(onnx_file))[0]
+    symbol_file = '%s-symbol.json' % model_name
+    params_file = '%s-0000.params' % model_name
+
+    sym, params = onnx_mxnet.import_model(onnx_file)
+    with open(os.path.join(model_path, symbol_file), 'w') as f:
+        f.write(sym.tojson())
+
+    save_dict = {('arg:%s' % k): v.as_in_context(mx.cpu()) for k, v in params.items()}
+    mx.nd.save(os.path.join(model_path, params_file), save_dict)
+    return symbol_file, params_file
+
+
+def find_unique(files, suffix):
+    match = [f for f in files if f.endswith(suffix)]
+    count = len(match)
+
+    if count == 0:
+        return None
+    elif count == 1:
+        return match[0]
+    else:
+        params = {
+            '.params': ('.params', 'parameter file'),
+            '-symbol.json': ('...-symbol.json', 'symbol file'),
+            '.onnx': ('.onnx', 'ONNX model file'),
+        }[suffix]
+
+        message = 'mxnet-model-export expects only one {} file. Please supply the single {} ' + \
+                  'file you wish to export.'.format(params[0], params[1])
+
+        raise ValueError(message)
+
+
+def validate_epoch_number(params_file):
+    if re.match(r'^\w+-\d+\.params$', params_file) is None:
+        raise ValueError(NO_EPOCH_NUMBER_MESSAGE.format(params_file))
+
+
+def validate_prefix_match(symbol_file, params_file):
+    symbol_prefix = symbol_file.replace('-symbol.json', '')
+    params_prefix = params_file.split('-')[0]  # safe because of preceding regex
+    if symbol_prefix != params_prefix:
+        raise ValueError(MODEL_PREFIX_MISMATCH_MESSAGE.format(symbol_file, params_file))
+
+
+def validate_model_files(model_path, onnx_file, params_file, symbol_file):
+    mask = 0
     if onnx_file:
-        import onnx_mxnet
-        model_name = os.path.splitext(os.path.split(onnx_file)[-1])[0]
-        sym, params = onnx_mxnet.import_model(onnx_file)
-        sym_file = open(os.path.join(model_path,'%s-symbol.json' % model_name), 'w')
-        sym_file.write(sym.tojson())
-        save_dict = {('arg:%s' % k) : v.as_in_context(mx.cpu()) for k, v in params.items()}
-        mx.nd.save(os.path.join(model_path,'%s-0000.params' % model_name), save_dict)
+        mask += 1
+    if params_file:
+        mask += 2
+    if symbol_file:
+        mask += 4
+
+    if mask == 0:
+        raise ValueError(NO_MODEL_FILES_MESSAGE.format(model_path))
+    if mask in [3, 5, 7]:
+        raise ValueError(MIXED_MODEL_FILES_MESSAGE.format(model_path))
+    if mask in [2, 4]:
+        raise ValueError(INCOMPLETE_MODULE_FILES_MESSAGE.format(model_path))
+    if mask == 6:
+        # an mxnet model
+        validate_epoch_number(params_file)
+        validate_prefix_match(symbol_file, params_file)
 
 
-def export_model(model_name, model_path, service_file=None):
-    '''Internal helper for the exporting model command line interface.
-    '''
-    convert_to_mxnet(model_path)
+def export_model(model_name, model_path, service_file=None, export_file=None):
+    """
+    Internal helper for the exporting model command line interface.
+    """
 
-    destination = os.getcwd()
-    
+    if export_file is None:
+        export_file = '{}/{}.model'.format(os.getcwd(), model_name)
+    assert not os.path.exists(export_file), "model file {} already exists.".format(export_file)
+
     if model_path.startswith('~'):
         model_path = os.path.expanduser(model_path)
-    
-    model_path = model_path + os.sep if model_path[-1] != os.sep else model_path
+
+    files = os.listdir(model_path)
+
+    onnx_file = find_unique(files, '.onnx')
+    symbol_file = find_unique(files, '-symbol.json')
+    params_file = find_unique(files, '.params')
+
+    validate_model_files(model_path, onnx_file, params_file, symbol_file)
+
+    if onnx_file:
+        symbol_file, params_file = convert_onnx_model(model_path, onnx_file)
+        files.remove(onnx_file)
+        files.extend([symbol_file, params_file])
 
     signature_file = validate_signature(model_path)
-    
     service_file = validate_service(model_path, service_file, signature_file)
+    if service_file not in files:
+        shutil.copyfile(service_file, os.path.join(model_path, os.path.basename(service_file)))
+        service_file = os.path.basename(service_file)
+        files.append(service_file)
 
-    symbol_file = validate_symbol(model_path)
-    
-    params_file = validate_params(model_path)
-    
     manifest = generate_manifest(symbol_file, params_file, service_file, signature_file, model_name)
-    manifest_file = os.path.join(model_path, MANIFEST_FILE_NAME)
-    with open(manifest_file, 'w') as m:
+    with open(os.path.join(model_path, MANIFEST_FILE_NAME), 'w') as m:
         json.dump(manifest, m, indent=4)
+    files.append(MANIFEST_FILE_NAME)
 
-    file_list = [signature_file, service_file, symbol_file, params_file, manifest_file]
-    
-    #add all the auxillary files    
-    for dirpath, _, filenames in os.walk(model_path):
-        for filename in filenames:
-            filename = os.path.join(model_path, filename)
-            if os.path.isfile(filename) and filename not in file_list:
-                onnx_extension = '.' + ONNX_TYPE
-                if onnx_extension in filename:
-                    continue
-                file_list.append(filename)
-    export_file = os.path.join(destination,'%s.model' % model_name)
+    with zipfile.ZipFile(export_file, 'w') as z:
+        for f in files:
+            z.write(os.path.join(model_path, f), f)
 
-    assert False == os.path.isfile(export_file), "%s.model already exists in %s directory." % (model_name, destination)
+    logger.info('Successfully exported model {} to file {}.'.format(model_name, export_file))
 
-    with zipfile.ZipFile(export_file, 'w') as zip_file:
-        for item in file_list:
-            zip_file.write(item, os.path.basename(item))
-
-    logger.info('Successfully exported %s model. Model file is located in %s directory.'
-          % (model_name, destination))
 
 def export():
     args = ArgParser.export_parser().parse_args()
     export_model(args.model_name, args.model_path, args.service_file_path)
 
-if __name__ =='__main__':
+
+if __name__ == '__main__':
     export()

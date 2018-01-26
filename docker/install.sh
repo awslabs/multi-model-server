33e3b67eed9a05fa7d5d8e34aa45849b44c5663b
jesterhazy <jonathan.esterhazy@gmail.com>
2018-01-26 11:14:15 -0800

    update docker scripts (#313)

--- docker/install.sh
+++ docker/install.sh
@@ -1,18 +1,13 @@
 #!/usr/bin/env bash
 
 apt-get update
-apt-get install -y build-essential libatlas-base-dev libopencv-dev graphviz
-apt-get install -y python-setuptools python-pip
-apt-get install -y openjdk-7-jdk
-apt-get install -y nginx
-
 apt-get install -y software-properties-common
 add-apt-repository -y ppa:certbot/certbot
 apt-get update
-apt-get install -y python-certbot-nginx
-apt-get install protobuf-compiler libprotoc-dev
 
-pip install --upgrade pip
-pip install gunicorn
-pip install gevent
-pip install mxnet-model-server
+apt-get install -y build-essential libatlas-base-dev libopencv-dev graphviz python-dev \
+    openjdk-8-jdk nginx protobuf-compiler libprotoc-dev python-certbot-nginx curl
+
+cd /tmp && curl -O https://bootstrap.pypa.io/get-pip.py && python get-pip.py
+
+pip install gevent gunicorn mxnet-model-server

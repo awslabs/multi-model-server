efcce0a9189969ba41f5e81c89940bbdcc082b7d
Khedia <khedia@8c85902de6e4.ant.amazon.com>
2018-03-09 14:46:12 -0800

    deleted redundant files

--- mms/mxnet_model_server.py
+++ mms/mxnet_model_server.py
@@ -156,18 +156,10 @@ class MMS(object):
 
             # Load models using registered model definitions
             registered_models = self.serving_frontend.get_registered_modelservices()
-            ModelClassDef = registered_models[model_class_name]
-            
-<<<<<<< HEAD
-            self.serving_frontend.load_models(models, ModelClassDef, self.gpu)
-                        
-=======
+            ModelClassDef = registered_models[model_class_name]                        
+
             self.serving_frontend.load_models(self.models, ModelClassDef, self.gpu)
             
-            #if len(self.args.models) > 5:
-            #    raise Exception('Model number exceeds our system limits: 5')
-            
->>>>>>> 6745e2b826d40018b93eef736e3aae8a721f4d8c
             # Setup endpoint
             openapi_endpoints = self.serving_frontend.setup_openapi_endpoints(self.host, self.port)
 

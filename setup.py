7398eaf716642fb9967aeaa62852e005186a151f
vrakesh <rakesh.nvasudev@gmail.com>
2018-04-27 18:17:54 -0700

    lower case conversion for OS

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ pkgs.append('tools')
 requirements = ['Flask', 'Pillow', 'requests', 'flask-cors',
                 'psutil', 'jsonschema', 'onnx-mxnet>=0.4.2', 'boto3', 'importlib2',
                 'fasteners']
-if platform.system() == 'Linux':
+if platform.system().lower() == 'linux':
     requirements = ['mxnet-cu90mkl>=1.1'] + requirements
 else:
     requirements = ['mxnet-mkl>=1.1'] + requirements

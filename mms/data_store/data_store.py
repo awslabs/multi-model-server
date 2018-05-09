13353ad151559216fff4b5e149f8c172c8e7c8f9
Charlie You <charlie@charlieyou.me>
2018-05-09 15:28:27 -0700

    Revert "addressed lint errors"
    This reverts commit c50c5de0adf63a7a64516fbdcf28612555688d57.

--- mms/data_store/data_store.py
+++ mms/data_store/data_store.py
@@ -17,7 +17,7 @@ from mms.data_store.hashmap import RedisHashMap
 from mms.data_store.queue import RedisQueue
 
 
-class DataStore(object):
+class DataStore:
     """
     DataStore abstracts the queue, hashmap, and associated codecs
     """
@@ -40,15 +40,15 @@ class DataStore(object):
         codec = self._get_codec(data_type)
         self.queue.push(model_name, codec.serialize(data))
 
-    def get(self, _id, data_type, timeout=60):
+    def get(self, id, data_type, timeout=60):
         codec = self._get_codec(data_type)
-        return codec.deserialize(self.hashmap.get(_id, timeout))
+        return codec.deserialize(self.hashmap.get(id, timeout))
 
     # performance could possibly be improved by using Redis Pipeliningg
     def set_batch(self, ids, data, data_type):
         codec = self._get_codec(data_type)
-        for _id, datum in zip(ids, data):
-            self.hashmap.put(_id, codec.serialize(datum))
+        for id, datum in zip(ids, data):
+            self.hashmap.put(id, codec.serialize(datum))
 
     @staticmethod
     def _get_codec(data_type):
@@ -57,4 +57,5 @@ class DataStore(object):
         elif data_type == "application/json":
             return JsonCodec
         else:
-            return None
+            # This should never happen, data type is checked upstream
+            return

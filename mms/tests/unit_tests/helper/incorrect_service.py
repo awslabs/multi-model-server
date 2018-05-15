db8ee93ca20be9a4bcbfe1a7bab6038f52675382
vrakesh <rakesh.nvasudev@gmail.com>
2018-05-15 14:48:49 -0700

    Added more comments to incorrect service

--- mms/tests/unit_tests/helper/incorrect_service.py
+++ mms/tests/unit_tests/helper/incorrect_service.py
@@ -8,9 +8,12 @@
 # express or implied. See the License for the specific language governing
 # permissions and limitations under the License.
 from mms.model_service.mxnet_vision_service import MXNetVisionService
+# Test will fail here
 import wrong_package
 
-
+''' This class will not be loaded by the test, since error is above in the service class
+    The test is testing if the failed import propogates up to the model service level.
+'''
 class DummyService(MXNetVisionService):
 
     def __init__(self, model_name, model_dir, manifest, gpu=None):

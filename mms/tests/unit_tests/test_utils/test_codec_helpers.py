391c02e2cc80c18564a3c908e8f3a9dabad925fb
Frank Liu <lufen@amazon.com>
2018-07-11 21:43:52 -0700

    Fix backened python3 compatiblity issues.

--- mms/tests/unit_tests/test_utils/test_codec_helpers.py
+++ mms/tests/unit_tests/test_utils/test_codec_helpers.py
@@ -9,11 +9,8 @@ from mms.utils.codec_helpers.codec import ModelWorkerCodecHelper
 class TestMXNetImageUtils(unittest.TestCase):
     def test_model_worker_encoder(self):
         msg = "Hello World!!"
-        assert base64.b64encode(msg) == ModelWorkerCodecHelper.encode_msg(u'base64', msg), \
+        assert base64.b64encode(msg).decode('utf-8') == ModelWorkerCodecHelper.encode_msg(u'base64', msg), \
             "base64 encoder not correct"
-
-        assert msg.encode('utf-8') == ModelWorkerCodecHelper.encode_msg('utf-8', msg), \
-            "UTF-8 encoder not correct"
 
     def test_invalid_encode(self):
         msg = "Hello World!!"

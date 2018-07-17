822dd1d51c6814397069f183bf16a14096b9a5e5
vrakesh <rakesh.nvasudev@gmail.com>
2018-07-16 18:45:15 -0700

    Made some suggested changes, code formatting

--- frontend/server/src/test/java/com/amazonaws/ml/mms/metrics/MetricManagerTest.java
+++ frontend/server/src/test/java/com/amazonaws/ml/mms/metrics/MetricManagerTest.java
@@ -1,10 +1,8 @@
 package com.amazonaws.ml.mms.metrics;
 
 import com.amazonaws.ml.mms.TestUtils;
-import java.security.GeneralSecurityException;
-
 import com.amazonaws.ml.mms.util.ConfigManager;
-import org.testng.Assert;
+import java.security.GeneralSecurityException;
 import org.testng.annotations.Test;
 
 public class MetricManagerTest {

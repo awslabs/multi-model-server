88dfa697f9da15d56484f62b95af3596249ad2f3
Rakesh Vasudevan <rakesh.nvasudev@gmail.com>
2018-07-23 11:07:29 -0700

    added backend metrics (#453)
    * merged backend metrics
    
    * Made some changes, and improvements
    
    * removed builtins
    
    * script to generate system level metrics
    
    * make pylint happy
    
    * Made changes suggested
    
    * Improved dimension level traversal information in code, moved more redundant code to hidden functions
    
    * Made changes to metric emission standard, followed styles from MetricLine wiki and old MMS implementation
    
    * pylint changes
    
    * Included an example to push to cloudwatch, removed request id from dimensions
    
    * Made requested changes
    
    * mms/examples/__init__.py
    
    * mms/model_service/model_service.py
    
    * made requsted changes
    
    * moved unit to metrics sub-package
    
    * minor changes to keys
    
    * Update buildspec.yml
    
    * Update buildspec.yml
    
    * Update system_metrics.py
    
    * Update system_metrics.py
    
    * Update system_metrics.py
    
    * Improved test exception handling

---
+++ mms/metrics/system_metrics.py
@@ -0,0 +1,74 @@
+# Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# Licensed under the Apache License, Version 2.0 (the "License").
+# You may not use this file except in compliance with the License.
+# A copy of the License is located at
+#     http://www.apache.org/licenses/LICENSE-2.0
+# or in the "license" file accompanying this file. This file is distributed
+# on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+# express or implied. See the License for the specific language governing
+# permissions  and limitations under the License.
+"""
+Module to collect system metrics for front-end
+"""
+import sys
+import types
+import json
+import socket
+
+import psutil
+from mms.metrics.metric import Metric
+from mms.metrics.metric_encoder import MetricEncoder
+from mms.metrics.dimension import Dimension
+
+system_metrics = []
+dimension = [Dimension('Level', 'Host'), Dimension('Host', socket.gethostname())]
+
+
+
+def cpu_utilization():
+    data = psutil.cpu_percent()
+    system_metrics.append(Metric('CPUUtilization', data, 'percent', dimension))
+
+
+def memory_used():
+    data = psutil.virtual_memory().used / (1024 * 1024)  # in MB
+    system_metrics.append(Metric('MemoryUsed', data, 'MB', dimension))
+
+
+def memory_available():
+    data = psutil.virtual_memory().available / (1024 * 1024)  # in MB
+    system_metrics.append(Metric('MemoryAvailable', data, 'MB', dimension))
+
+
+def memory_utilization():
+    data = psutil.virtual_memory().percent
+    system_metrics.append(Metric('MemoryUtilization', data, 'percent', dimension))
+
+
+def disk_used():
+    data = psutil.disk_usage('/').used / (1024 * 1024 * 1024)  # in GB
+    system_metrics.append(Metric('DiskUsage', data, 'GB', dimension))
+
+
+def disk_utilization():
+    data = psutil.disk_usage('/').percent
+    system_metrics.append(Metric('DiskUtilization', data, 'percent', dimension))
+
+
+def disk_available():
+    data = psutil.disk_usage('/').free / (1024 * 1024 * 1024)  # in GB
+    system_metrics.append(Metric('DiskAvailable', data, 'GB', dimension))
+
+
+def collect_all(mod):
+    members = dir(mod)
+    for i in members:
+        value = getattr(mod, i)
+        if isinstance(value, types.FunctionType) and value.__name__ != 'collect_all':
+            value()
+    print(json.dumps(system_metrics, separators=(',', ':'), cls=MetricEncoder))
+    sys.stdout.flush()
+
+
+if __name__ == '__main__':
+    collect_all(sys.modules[__name__])
---
+++ mms/tests/unit_tests/test_beckend_metric.py
@@ -0,0 +1,95 @@
+from mms.metrics.metrics_store import MetricsStore
+from mms.metrics.dimension import Dimension
+from mms.model_service_worker import emit_metrics
+import pytest
+
+def get_model_key(name, unit, req_id, model_name):
+    dimensions = list()
+    dimensions.append(Dimension("ModelName", model_name))
+    dimensions.append(Dimension("Level", "Model"))
+    dim_str = [name, unit, str(req_id)] + [str(d) for d in dimensions]
+    return '-'.join(dim_str)
+
+
+def get_error_key(name, unit):
+    dimensions=list()
+    dimensions.append(Dimension("Level", "Error"))
+    dim_str = [name, unit, 'None'] + [str(d) for d in dimensions]
+    return '-'.join(dim_str)
+
+
+def test_metrics(capsys):
+    """
+    Test if metric classes methods behave as expected
+    Also checks global metric service methods
+    """
+    # Create a batch of request ids
+    request_ids = {0 : 'abcd', 1 :"xyz", 2 : "qwerty", 3 : "hjshfj" }
+
+    model_name = "dummy model"
+
+    # Create a metrics objects
+    metrics = MetricsStore(request_ids, model_name)
+
+    # Counter tests
+    metrics.add_counter('CorrectCounter', 1, 1)
+    test_metric = metrics.cache[get_model_key('CorrectCounter', 'count', 'xyz', model_name)]
+    assert 'CorrectCounter' == test_metric.name
+    metrics.add_counter('CorrectCounter', 1, 1)
+    metrics.add_counter('CorrectCounter', 1, 3)
+    metrics.add_counter('CorrectCounter', 1)
+    print(metrics.cache)
+    test_metric = metrics.cache[get_model_key('CorrectCounter', 'count', 'ALL', model_name)]
+    assert 'CorrectCounter' == test_metric.name
+    metrics.add_counter('CorrectCounter', 3)
+    test_metric = metrics.cache[get_model_key('CorrectCounter', 'count', 'xyz', model_name)]
+    assert test_metric.value == 2
+    test_metric = metrics.cache[get_model_key('CorrectCounter', 'count', 'hjshfj', model_name)]
+    assert test_metric.value == 1
+    test_metric = metrics.cache[get_model_key('CorrectCounter', 'count', 'ALL', model_name)]
+    assert  test_metric.value == 4
+    # Check what is emitted is correct
+    emit_metrics(metrics.store)
+    out, err = capsys.readouterr()
+    assert '"dimensions":[' in out
+    assert '"value":"Model"' in out
+
+
+    # Adding other types of metrics
+    # Check for time metric
+    with pytest.raises(Exception) as e_info:
+        metrics.add_time('WrongTime', 20, 1, 'ns')
+        assert "the unit for a timed metric should be" in str(e_info)
+
+    metrics.add_time('CorrectTime', 20, 2, 's')
+    metrics.add_time('CorrectTime', 20, 0)
+    test_metric = metrics.cache[get_model_key('CorrectTime', 'ms', 'abcd', model_name)]
+    assert test_metric.value == 20
+    assert test_metric.unit == 'Milliseconds'
+    test_metric = metrics.cache[get_model_key('CorrectTime', 's', 'qwerty', model_name)]
+    assert test_metric.value == 20
+    assert test_metric.unit == 'Seconds'
+    # Size based metrics
+    with pytest.raises(Exception) as e_info:
+        metrics.add_time('WrongSize', 20, 1, 'TB')
+        assert "The unit for size based metric is one of" in str(e_info)
+
+    metrics.add_size('CorrectSize', 200, 0, 'GB')
+    metrics.add_size('CorrectSize', 10, 2)
+    test_metric = metrics.cache[get_model_key('CorrectSize', 'GB', 'abcd', model_name)]
+    assert test_metric.value == 200
+    assert test_metric.unit == 'Gigabytes'
+    test_metric = metrics.cache[get_model_key('CorrectSize', 'MB', 'qwerty', model_name)]
+    assert test_metric.value == 10
+    assert test_metric.unit == 'Megabytes'
+
+    # Check a percentage metric
+    metrics.add_percent('CorrectPercent', 20.0, 3)
+    test_metric = metrics.cache[get_model_key('CorrectPercent', 'percent', 'hjshfj', model_name)]
+    assert test_metric.value == 20.0
+    assert test_metric.unit == 'Percent'
+
+    # Check a error metric
+    metrics.add_error('CorrectError', 'Wrong values')
+    test_metric = metrics.cache[get_error_key('CorrectError', '')]
+    assert test_metric.value == 'Wrong values'

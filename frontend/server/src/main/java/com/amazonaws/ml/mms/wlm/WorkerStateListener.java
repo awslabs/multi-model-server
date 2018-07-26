180e35704a12dc113ddac15c64d0d63676ef0d7f
vrakesh <rakesh.nvasudev@gmail.com>
2018-07-26 16:53:55 -0700

    Added Synchronous option to scale call
    1. Added a Worker Listener to worker thread
    2. Uses completablefuture class to achieve this.
    3. Improved states in listener, removed multiple counters.
    4. Added Atomic Counter
    5. Added exception handling in applyThem
    6. Code quality improvements.

---
+++ frontend/server/src/main/java/com/amazonaws/ml/mms/wlm/WorkerStateListener.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance
+ * with the License. A copy of the License is located at
+ *
+ * http://aws.amazon.com/apache2.0/
+ *
+ * or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES
+ * OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions
+ * and limitations under the License.
+ */
+package com.amazonaws.ml.mms.wlm;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.atomic.AtomicInteger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class WorkerStateListener {
+
+    public static final int WORKER_STARTED = 1;
+    public static final int WORKER_MODEL_LOADED = 2;
+    public static final int WORKER_STOPPED = 3;
+    public static final int WORKER_ERROR = 4;
+    private static final Logger logger = LoggerFactory.getLogger(WorkerStateListener.class);
+    private CompletableFuture<Boolean> future;
+    private AtomicInteger count;
+
+    public WorkerStateListener(CompletableFuture<Boolean> future, int count) {
+        this.future = future;
+        this.count = new AtomicInteger(count);
+    }
+
+    public void notifyChangeState(int state) {
+        logger.debug("Current state is", state);
+        // Update success and fail counts
+        if (state == WORKER_MODEL_LOADED) {
+            if (count.decrementAndGet() == 0) {
+                future.complete(Boolean.TRUE);
+            }
+        }
+        if (state == WORKER_ERROR || state == WORKER_STOPPED) {
+            future.complete(Boolean.FALSE);
+        }
+    }
+}

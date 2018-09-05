54963dcb30231a947903d6125bc06312e713f2f8
Frank Liu <lufen@amazon.com>
2018-09-05 13:53:17 -0700

    Add warnings to deprecated modules.

--- mms/utils/mxnet/__init__.py
+++ mms/utils/mxnet/__init__.py
@@ -11,3 +11,7 @@
 """
 MXNet Utils
 """
+import warnings
+
+warnings.warn("Module mms.utils.mxnet is deprecated, please avoid using mms internal modules.",
+              DeprecationWarning, stacklevel=2)

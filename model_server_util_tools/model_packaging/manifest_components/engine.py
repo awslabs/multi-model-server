2a54836ffd7aea41e9d6fc2df76dfaa1f4ae51fd
Piyush Ghai <ghai.8@osu.edu>
2018-09-06 12:15:28 -0700

    Added classes for manifest objects

---
+++ model_server_util_tools/model_packaging/manifest_components/engine.py
@@ -0,0 +1,44 @@
+# Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# Licensed under the Apache License, Version 2.0 (the "License").
+# You may not use this file except in compliance with the License.
+# A copy of the License is located at
+#     http://www.apache.org/licenses/LICENSE-2.0
+# or in the "license" file accompanying this file. This file is distributed
+# on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+# express or implied. See the License for the specific language governing
+# permissions and limitations under the License.
+
+import json
+from enum import Enum
+from model_server_util_tools.model_packaging.model_packaging_error import ModelPackagingError
+from model_server_util_tools.model_packaging.model_packaging_error_codes import ModelPackagingErrorCodes
+
+
+class EngineType(Enum):
+    MXNET = "MXNet"
+
+    #TODO Add more engines here as and when MMS supports more DL Frameworks
+
+
+class Engine(object):
+
+    def __init__(self, engine_name, engine_version):
+        try:
+            self.engine_name = EngineType(engine_name)
+            self.engine_version = engine_version
+        except ValueError as err:
+            raise ModelPackagingError(ModelPackagingErrorCodes.INVALID_ENGINE_NAME, repr(err))
+
+        self.engine_dict = self.__to_dict__()
+
+    def __to_dict__(self):
+        engine_dict = dict()
+        engine_dict['engineName'] = self.engine_name.value
+        engine_dict['engineVersion'] = self.engine_version
+        return engine_dict
+
+    def __str__(self):
+        return json.dumps(self.engine_dict)
+
+    def __repr__(self):
+        return json.dumps(self.engine_dict)
---
+++ model_server_util_tools/model_packaging/manifest_components/manifest.py
@@ -0,0 +1,62 @@
+# Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# Licensed under the Apache License, Version 2.0 (the "License").
+# You may not use this file except in compliance with the License.
+# A copy of the License is located at
+#     http://www.apache.org/licenses/LICENSE-2.0
+# or in the "license" file accompanying this file. This file is distributed
+# on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+# express or implied. See the License for the specific language governing
+# permissions and limitations under the License.
+
+import json
+from enum import Enum
+from model_server_util_tools.model_packaging.model_packaging_error import ModelPackagingError
+from model_server_util_tools.model_packaging.model_packaging_error_codes import ModelPackagingErrorCodes
+
+
+class RuntimeType(Enum):
+
+    PYTHON2_7 = "python2.7"
+    PYTHON3_6 = "python3.6"
+
+    # TODO : Add more runtimes here when we support more runtimes such as Java/Go/Scala etc..
+
+
+class Manifest(object):
+
+    def __init__(self, runtime, engine, model, publisher, specification_version, implementation_version,
+                 model_server_version, license, description, user_data):
+        try:
+            self.runtime = RuntimeType(runtime)
+        except ValueError as err:
+            raise ModelPackagingError(ModelPackagingErrorCodes.INVALID_RUNTIME_TYPE, repr(err))
+        self.engine = engine
+        self.model = model
+        self.publisher = publisher
+        self.specification_version = specification_version
+        self.implementation_version = implementation_version
+        self.model_server_version = model_server_version
+        self.license = license
+        self.description = description
+        self.user_data = user_data
+        self.manifest_dict = self.__to_dict__()
+
+    def __to_dict__(self):
+        manifest_dict = dict()
+        manifest_dict['runtime'] = self.runtime.value
+        manifest_dict['engine'] = str(self.engine)
+        manifest_dict['model'] = str(self.model)
+        manifest_dict['publisher'] = str(self.publisher)
+        manifest_dict['license'] = self.license
+        manifest_dict['modelServerVersion'] = self.model_server_version
+        manifest_dict['description'] = self.description
+        manifest_dict['implementationVersion'] = self.implementation_version
+        manifest_dict['specificationVersion'] = self.specification_version
+        manifest_dict['userData'] = self.user_data
+        return manifest_dict
+
+    def __str__(self):
+        return json.dumps(self.manifest_dict)
+
+    def __repr__(self):
+        return json.dumps(self.manifest_dict)

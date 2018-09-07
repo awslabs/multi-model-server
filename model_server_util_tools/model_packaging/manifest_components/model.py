fa120a3689757498664599484e9e152dc1d5e436
Piyush Ghai <ghai.8@osu.edu>
2018-09-07 11:15:08 -0700

    Removed error handling and replaced with sys.exits()

--- model_server_util_tools/model_packaging/manifest_components/model.py
+++ model_server_util_tools/model_packaging/manifest_components/model.py
@@ -18,21 +18,29 @@ class Model(object):
     as the entry point into the service code through the handler property
     """
 
-    def __init__(self, model_name, description, model_version, extensions, handler):
+    def __init__(self, model_name, handler, description=None, model_version=None, extensions=None):
         self.model_name = model_name
         self.description = description
         self.model_version = model_version
-        self.extentions = extensions
+        self.extensions = extensions
         self.handler = handler
         self.model_dict = self.__to_dict__()
 
     def __to_dict__(self):
         model_dict = dict()
+
         model_dict['modelName'] = self.model_name
-        model_dict['description'] = self.description
-        model_dict['modelVersion'] = self.model_version
-        model_dict['extensions'] = self.extensions
+
         model_dict['handler'] = self.handler
+
+        if self.description is not None:
+            model_dict['description'] = self.description
+
+        if self.model_version is not None:
+            model_dict['modelVersion'] = self.model_version
+
+        if self.extensions is not None:
+            model_dict['extensions'] = self.extensions
 
         return model_dict
 

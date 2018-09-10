a2ba59572c30315d60591999fe4413ff866d1007
Piyush Ghai <ghai.8@osu.edu>
2018-09-10 13:50:07 -0700

    Added more unit tests

--- model_server_tools/model_packaging/tests/unit_tests/test_model_packaging.py
+++ model_server_tools/model_packaging/tests/unit_tests/test_model_packaging.py
@@ -8,4 +8,53 @@
 # express or implied. See the License for the specific language governing
 # permissions and limitations under the License.
 
-import json
+import pytest
+from mock import Mock, mock_open, patch
+from collections import namedtuple
+from model_server_tools.model_packaging.manifest_components.engine import EngineType
+from model_server_tools.model_packaging.manifest_components.manifest import RuntimeType
+from model_server_tools.model_packaging.model_packaging import generate_model_archive, export_model
+from model_server_tools.model_packaging.model_packaging_utils import ModelExportUtils
+
+
+# noinspection PyClassHasNoInit
+class TestModelPackaging:
+
+    class Namespace:
+        def __init__(self, **kwargs):
+            self.__dict__.update(kwargs)
+
+    author = 'ABC'
+    email = 'ABC@XYZ.com'
+    engine = EngineType.MXNET.value
+    model_name = 'my-model'
+    model_path = 'my-model/'
+    handler = 'a.py::my-awesome-func'
+
+    args = Namespace(author=author, email=email, engine=engine, model_name=model_name, handler=handler,
+                     runtime=RuntimeType.PYTHON2_7.value, model_path=model_path, force=False)
+
+    @pytest.fixture()
+    def patches(self, mocker):
+        Patches = namedtuple('Patches', ['arg_parse', 'export_utils', 'export_method'])
+        patch = Patches(mocker.patch('model_server_tools.model_packaging.model_packaging.ArgParser'),
+                        mocker.patch('model_server_tools.model_packaging.model_packaging.ModelExportUtils'),
+                        mocker.patch('model_server_tools.model_packaging.model_packaging.export_model'))
+
+        return patch
+
+    def test_gen_model_archive(self, patches):
+        patches.arg_parse.export_model_args_parser.parse_args.return_value = self.args
+        generate_model_archive()
+        patches.export_method.assert_called()
+
+    def test_export_model_method(self, patches):
+
+        patches.export_utils.check_mar_already_exists.return_value = '/Users/ghaipiyu/'
+        patches.export_utils.check_custom_model_types.return_value = '/Users/ghaipiyu', ['a.txt', 'b.txt']
+        patches.export_utils.zip.return_value = None
+
+        export_model(self.args, ModelExportUtils.generate_manifest_json(self.args))
+        patches.export_utils.check_model_name_regex.assert_called()
+        patches.export_utils.zip.assert_called()
+        patches.export_utils.clean_temp_files.assert_called()

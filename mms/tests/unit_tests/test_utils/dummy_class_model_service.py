e7cd2d68862dc9f53912ee68fb6bd532abd846a4
Frank Liu <lufen@amazon.com>
2018-09-11 11:08:33 -0700

    Resolve Pycharm warnings.

--- mms/tests/unit_tests/test_utils/dummy_class_model_service.py
+++ mms/tests/unit_tests/test_utils/dummy_class_model_service.py
@@ -13,11 +13,13 @@ Dummy custom service which is class based
 """
 
 
+# noinspection PyUnusedLocal
 class CustomService(object):
 
     def initialize(self, context):
         pass
 
+    # noinspection PyMethodMayBeStatic
     def handle(self, data, context):
         from mms.context import Context
         return ["OK"]
--- mms/tests/unit_tests/test_utils/dummy_func_model_service.py
+++ mms/tests/unit_tests/test_utils/dummy_func_model_service.py
@@ -15,5 +15,6 @@ Dummy custom service which is function based
 from mms.context import Context
 
 
+# noinspection PyUnusedLocal
 def infer(data, context):
     return isinstance(context, Context)

995a052f4c6219f77e3099c439e81a3f26cceb5e
Piyush Ghai <ghai.8@osu.edu>
2018-09-17 11:02:02 -0700

    Fixed Python 3 packaging issue for model-archive tool

--- model_archiver/model_archiver/__init__.py
+++ model_archiver/model_archiver/__init__.py
@@ -1,4 +1,4 @@
-# Copyright 2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # Licensed under the Apache License, Version 2.0 (the "License").
 # You may not use this file except in compliance with the License.
 # A copy of the License is located at
@@ -7,3 +7,11 @@
 # on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
 # express or implied. See the License for the specific language governing
 # permissions and limitations under the License.
+
+"""
+This module does the following:
+Exports the model folder to generate a Model Archive file out of it in .mar format
+"""
+from . import version
+
+__version__ = version.__version__

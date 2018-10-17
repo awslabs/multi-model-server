9f2cd4c9dbf21d00e0a3b19948d53e382e2f11c7
Frank Liu <lufen@amazon.com>
2018-10-17 13:08:24 -0700

    Improve error handling in examples template code.

--- examples/model_service_template/mxnet_vision_service.py
+++ examples/model_service_template/mxnet_vision_service.py
@@ -11,6 +11,7 @@
 """
 MXNetVisionService defines a MXNet base vision service
 """
+import logging
 
 from mxnet_model_service import MXNetModelService
 from mxnet_utils import image, ndarray
@@ -25,6 +26,15 @@ class MXNetVisionService(MXNetModelService):
     """
 
     def preprocess(self, request):
+        """
+        Decode all input images into ndarray.
+
+        Note: This implementation doesn't properly handle error cases in batch mode,
+        If one of the input images is corrupted, all requests in the batch will fail.
+
+        :param request:
+        :return:
+        """
         img_list = []
         param_name = self.signature['inputs'][0]['data_name']
         input_shape = self.signature['inputs'][0]['data_shape']
@@ -37,15 +47,29 @@ class MXNetVisionService(MXNetModelService):
             if img is None:
                 img = data.get("data")
 
+            if img is None or len(img) == 0:
+                self.error = "Empty image input"
+                return None
+
             # We are assuming input shape is NCHW
             [h, w] = input_shape[2:]
-            img_arr = image.read(img)
+
+            try:
+                img_arr = image.read(img)
+            except Exception as e:
+                logging.warn(e, exc_info=True)
+                self.error = "Corrupted image input"
+                return None
+
             img_arr = image.resize(img_arr, w, h)
             img_arr = image.transform_shape(img_arr)
             img_list.append(img_arr)
         return img_list
 
     def postprocess(self, data):
+        if self.error is not None:
+            return [self.error] * self._batch_size
+
         assert hasattr(self, 'labels'), \
             "Can't find labels attribute. Did you put synset.txt file into " \
             "model archive or manually load class label file in __init__?"

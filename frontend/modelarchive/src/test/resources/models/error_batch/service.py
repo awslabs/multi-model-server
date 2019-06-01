f73d8c2fa53578abb9df8c435163940cddea5577
Dantu <vamshidhar.d.k@gmail.com>
2019-06-01 11:30:12 -0700

    Redid context APIs

--- frontend/modelarchive/src/test/resources/models/error_batch/service.py
+++ frontend/modelarchive/src/test/resources/models/error_batch/service.py
@@ -16,5 +16,5 @@ InvalidService defines a invalid model handler for testing purpose.
 def handle(data, context):
     # This model is created to test reporting of an error in a batch of requests
     if data:
-        context.set_http_response_status(code=507, idx=0)
+        context.set_response_status(code=507, idx=0)
     return ["Invalid response"]

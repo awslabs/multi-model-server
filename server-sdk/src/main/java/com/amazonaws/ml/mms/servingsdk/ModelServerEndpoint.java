91d275214c48d1532631d00263e7389658661d2a
Dantu <vamshidhar.d.k@gmail.com>
2019-06-13 10:55:49 -0700

    Serving SDK commit

---
+++ server-sdk/src/main/java/com/amazonaws/ml/mms/servingsdk/ModelServerEndpoint.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance
+ * with the License. A copy of the License is located at
+ *
+ * http://aws.amazon.com/apache2.0/
+ *
+ * or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES
+ * OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions
+ * and limitations under the License.
+ */
+package com.amazonaws.ml.mms.servingsdk;
+
+import com.amazonaws.ml.mms.servingsdk.http.Request;
+import com.amazonaws.ml.mms.servingsdk.http.Response;
+
+import java.io.IOException;
+
+/**
+ * This class defines the abstract class for ModelServerEndpoint
+ */
+public abstract class ModelServerEndpoint {
+    /**
+     * This method is called when a HTTP GET method is invoked for the defined custom model server endpoint
+     * @param req - Incoming request
+     * @param res - Outgoing response
+     * @param ctx - ModelServer's context which defines the current model-server system information
+     * @throws IOException
+     */
+    public void doGet(Request req, Response res, Context ctx) throws IOException {
+        throw new ModelServerEndpointException("No implementaion found .. Default implementation invoked");
+    }
+}

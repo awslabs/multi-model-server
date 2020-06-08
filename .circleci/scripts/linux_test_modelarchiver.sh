decd8a1d723c4bdd6238c9202cdec64dedc5cddf
Prashant Sail <psail4444@gmail.com>
2020-06-08 15:37:57 +0530

    save test results

--- .circleci/scripts/linux_test_modelarchiver.sh
+++ .circleci/scripts/linux_test_modelarchiver.sh
@@ -6,11 +6,11 @@ cd model-archiver/
 pylint -rn --rcfile=./model_archiver/tests/pylintrc model_archiver/.
 
 # Execute python unit tests
-python -m pytest model_archiver/tests/unit_tests
+python -m pytest --cov-report html:results_units --cov=./ model_archiver/tests/unit_tests
 
 
 # Install model archiver module
 pip install .
 
 # Execute integration tests
-pytest model_archiver/tests/integ_tests
+python -m pytest --cov-report html:results_integration --cov=./ model_archiver/tests/integ_tests

1e69b37911914bd4d5ca28c47d7212746fe0bb02
Prashant Sail <psail4444@gmail.com>
2020-06-09 14:01:11 +0530

    performce taurus with cci

--- .circleci/scripts/linux_test_taurus.sh
+++ .circleci/scripts/linux_test_taurus.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+set -e
+
+# Start MMS server
 multi-model-server --start >> mms.log 2>&1
 sleep 10
 
 #cd taurus
 cd benchmarks/monitoring
+
+# Install dependencies
+pip install requirements.txt
 
 curl -O https://s3.amazonaws.com/model-server/inputs/kitten.jpg
 ./run_perfomance_suite.py --artifacts-dir=/tmp/mms-performance-regression/ --jmeter-path=/opt/apache-jmeter-5.3/bin/jmeter

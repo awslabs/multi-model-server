a79c159877b0008a1c5bd8ac7af2fb88b48f0592
Prashant Sail <psail4444@gmail.com>
2020-06-09 14:27:29 +0530

    tweaked for store_test_results

--- .circleci/scripts/linux_test_taurus.sh
+++ .circleci/scripts/linux_test_taurus.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 set -e
+ARTIFACTS_DIR='/tmp/mms-performance-regression/'
+RESULT_DIR=$ARTIFACTS_DIR'/report/performance/'
+JMETER_PATH='/opt/apache-jmeter-5.3/bin/jmeter'
 
 # Start MMS server
 multi-model-server --start >> mms.log 2>&1
@@ -12,7 +15,13 @@ cd benchmarks/monitoring
 # Install dependencies
 pip install -r requirements.txt
 
+# Execute performance test suite
 curl -O https://s3.amazonaws.com/model-server/inputs/kitten.jpg
-./run_perfomance_suite.py --artifacts-dir=/tmp/mms-performance-regression/ --jmeter-path=/opt/apache-jmeter-5.3/bin/jmeter
+./run_perfomance_suite.py --artifacts-dir=$ARTIFACTS_DIR --jmeter-path=$JMETER_PATH
 
+# Stop server
 multi-model-server --stop >> mms.log 2>&1
+
+# Collect and store test results in result directory to be picked up by CircleCI
+mkdir -p $RESULT_DIR
+cp $ARTIFACTS_DIR'/junit.xml' $RESULT_DIR

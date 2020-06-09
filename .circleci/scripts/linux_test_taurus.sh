61a0cdddd25b06781b63cc9b436b8b4506377e24
Prashant Sail <psail4444@gmail.com>
2020-06-09 14:40:14 +0530

    store test results and exit with exit_code

--- .circleci/scripts/linux_test_taurus.sh
+++ .circleci/scripts/linux_test_taurus.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-set -e
 ARTIFACTS_DIR='/tmp/mms-performance-regression/'
 RESULT_DIR=$ARTIFACTS_DIR'/report/performance/'
 JMETER_PATH='/opt/apache-jmeter-5.3/bin/jmeter'
@@ -15,9 +14,10 @@ cd benchmarks/monitoring
 # Install dependencies
 pip install -r requirements.txt
 
-# Execute performance test suite
+# Execute performance test suite and store exit code
 curl -O https://s3.amazonaws.com/model-server/inputs/kitten.jpg
 ./run_perfomance_suite.py --artifacts-dir=$ARTIFACTS_DIR --jmeter-path=$JMETER_PATH
+EXIT_CODE=$?
 
 # Stop server
 multi-model-server --stop >> mms.log 2>&1
@@ -25,3 +25,6 @@ multi-model-server --stop >> mms.log 2>&1
 # Collect and store test results in result directory to be picked up by CircleCI
 mkdir -p $RESULT_DIR
 cp $ARTIFACTS_DIR'/junit.xml' $RESULT_DIR
+
+# Exit with the same error code as that of test execution
+exit EXIT_CODE

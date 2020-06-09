ab8525d9f5e42f2fda63af6015d36a5f4ccc7e9a
Prashant Sail <psail4444@gmail.com>
2020-06-09 12:53:51 +0530

    updated taurus test suite

--- .circleci/scripts/linux_test_taurus.sh
+++ .circleci/scripts/linux_test_taurus.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
 
-multi-model-server --start \
-                   --models squeezenet=https://s3.amazonaws.com/model-server/model_archive_1.0/squeezenet_v1.1.mar \
-                   >> mms.log 2>&1
-sleep 90
+multi-model-server --start >> mms.log 2>&1
+sleep 10
 
-cd taurus
+#cd taurus
+cd benchmarks/monitoring
 
 curl -O https://s3.amazonaws.com/model-server/inputs/kitten.jpg
-bzt -o modules.jmeter.path=/opt/apache-jmeter-5.3/bin/jmeter \
-    -o settings.artifacts-dir=/tmp/cci-taurus/ \
-    -o modules.console.disable=true \
-    imageInputModelPlan.jmx.yaml \
-    -report
+./run_performance_suite.py --artifacts-dir=/tmp/cci-taurus/
 
-multi-model-server --stop
+multi-model-server --stop >> mms.log 2>&1

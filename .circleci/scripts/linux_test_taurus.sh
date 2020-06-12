8241daf1378a1dc4dd1e0e37e2ed0b7abdcbe3ef
Prashant Sail <psail4444@gmail.com>
2020-06-12 20:44:34 +0530

    python 3.6 for local install for bzt

--- .circleci/scripts/linux_test_taurus.sh
+++ .circleci/scripts/linux_test_taurus.sh
@@ -7,6 +7,15 @@ sleep 90
 
 cd performance_regression
 
+# Hack to use python 3.6.5 for bzt installation and execution on a Python 2.7 environment
+# ToDo: Update condition to be more specific - check if python 2 is installed by default
+if hash pyenv 2>/dev/null; then
+  pyenv local 3.6.5
+fi
+
+# Install dependencies
+pip install bzt
+
 curl -O https://s3.amazonaws.com/model-server/inputs/kitten.jpg
 bzt -o modules.jmeter.path=/opt/apache-jmeter-5.3/bin/jmeter \
     -o settings.artifacts-dir=/tmp/mms-performance-regression/ \

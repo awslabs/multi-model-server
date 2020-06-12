c139ae42f70427c52645ed4fd24dad0a15fb4655
Prashant Sail <psail4444@gmail.com>
2020-06-12 16:59:40 +0530

    Updated Readme, local exec scripts, image SHA

--- .circleci/README.md
+++ .circleci/README.md
@@ -4,14 +4,20 @@ Model Server uses CircleCI for builds. This folder contains the config and scrip
 ## config.yml
 _config.yml_ contains MMS build logic which will be used by CircleCI.
 
-## Jobs
-Currently following jobs are executed under "_**build_and_test***_" workflow
+## Workflows and Jobs
+Currently, following _workflows_ are available -
+1. **build_and_test_python27**
+2. **build_and_test_python36**
+
+Following _jobs_ are executed under each workflow
 1. **build** - Builds _frontend/model-server.jar_ and executes tests from gradle
 2. **modelarchiver** - Builds and tests modelarchiver module
 3. **python_tests** - Executes pytests from _mms/tests/unit_tests/_
 4. **benchmark** - Executes latency benchmark using resnet-18 model
 5. (NEW!) **api_tests** - Executes newman test suite for API testing
 6. (NEW!) **performance_regression** - Executes performance tests using taurus/jmeter 
+
+> Checkout _workflows_ and _jobs_ section in _config.yml_ for an up to date list 
 
 ## scripts
 Instead of using inline commands inside _config.yml_, job steps are configured as shell scripts.  
@@ -32,16 +38,24 @@ Following files in the _images_ folder are used to create the docker images
 To make it easy for developers to debug build issues locally, MMS supports CircleCI cli for running a job in a container on your machine.
 
 #### Dependencies
-1. CircleCI cli from - https://circleci.com/docs/2.0/local-cli/#installation
+1. CircleCI cli [Quick Install](https://circleci.com/docs/2.0/local-cli/#quick-installation)
 2. PyYAML (pip install PyYaml)
-3. docker
+3. docker (installed and running)
 
 #### Command
 Developers can use the following command to build MMS locally:  
 **_./run_circleci_tests.py <workflow_name> <job_name>_**
 ```bash
 $ cd multi-model-server
-$ ./run_circleci_tests.py build_and_test python_tests
+$ ./run_circleci_tests.py build_and_test_python27 python_tests
+$ ./run_circleci_tests.py build_and_test_python36 api_tests
 ```
+
+###### Checklist
+> 1. Make sure docker is running before you start local execution.  
+> 2. Docker containers to have **at least 4GB RAM, 2 CPU**.  
+> 3. If you are on a network with low bandwidth availability, pull the images beforehand using -  
+> docker pull prashantsail/mms-build:python3.6@sha256:ea8ad58cc1740587f062d3b149175563e783d420d61ff242f9280a2550457853  
+> docker pull prashantsail/mms-build:python2.7@sha256:9d18782939652917bbcc8b0181639529f582560dd42589a965e59708e9fd6306  
 
 > To avoid Pull Request build failures on github, developers should always make sure that their local builds pass.
--- run_circleci_tests.py
+++ run_circleci_tests.py
@@ -20,7 +20,8 @@ args = parser.parse_args()
 workflow = args.workflow
 job = args.job
 
-circleci_config_file = '.circleci/config.yml'
+cci_config_file = '.circleci/config.yml'
+sw_cci_config_file = '.circleci/sw_config.yml'
 processed_file = '.circleci/processed.yml'
 xformed_file = '.circleci/xformed.yml'
 processed_config = {}
@@ -28,8 +29,26 @@ xformed_config = {}
 xformed_job_name = 'mms_xformed_job'
 blacklisted_steps = ['persist_to_workspace', 'attach_workspace', 'store_artifacts']
 
+# Generate a new CircleCI config which only has ONLY one workflow (workflow name from cli args)
+# WHY ?
+# > In case of multiple workflows (py27, py36, etc), CircleCI updates the job names(myjob -> myjob-1) while creating
+# > processed.yml. This causes mismatch between 'job name from cli args' and 'job name in processed yaml'
+# > As a workaround, we pre-process config to only have 1 workflow. This ensures unchanged job names in process.yml
+with open(cci_config_file) as f1stream:
+    try:
+        config = yaml.safe_load(f1stream)
+        single_workflow_config = {
+            'version': config['workflows']['version'],
+            workflow: config['workflows'][workflow]
+        }
+        config['workflows'] = single_workflow_config
+        with open(sw_cci_config_file, 'w+') as f2stream:
+            yaml.dump(config, f2stream)
+    except yaml.YAMLError as err:
+        print(err)
+
 # Create processed YAML using circleci cli's 'config process' commands
-subprocess.check_call('circleci config process {} > {}'.format(circleci_config_file, processed_file), shell=True)
+subprocess.check_call('circleci config process {} > {}'.format(sw_cci_config_file, processed_file), shell=True)
 
 # Read the processed config
 with open(processed_file) as fstream:
@@ -40,31 +59,35 @@ with open(processed_file) as fstream:
 
 jobs_in_workflow = processed_config['workflows'][workflow]['jobs']
 
+
 # Recursively iterate over jobs in the workflow to generate an ordered list of parent jobs
-def getJobsToExec(job_name):
+def get_jobs_to_exec(job_name):
     result = []
     for job in jobs_in_workflow :
-        if(isinstance(job, str) and job_name == job):
+        if isinstance(job, str) and job_name == job:
             # We have reached a root job (outter most parent), no further traversal
             break
-        elif(isinstance(job, dict) and job_name == list(job)[0]):
+        elif isinstance(job, dict) and job_name == list(job)[0]:
             # Find all parent jpbs, recurse to find their respective ancestors
             parent_jobs = job[job_name].get('requires',[])
-            for pjob in parent_jobs :
-                result += getJobsToExec(pjob)
+            for pjob in parent_jobs:
+                result += get_jobs_to_exec(pjob)
             break
-    return (result + [job_name])
+    return result + [job_name]
 
-# The list might contain duplicate parent jobs due to muliple fan-ins like config; remove the duplicates
-jobs_to_exec = list(OrderedDict.fromkeys(getJobsToExec(job)))
+
+# The list might contain duplicate parent jobs due to multiple fan-ins like config; remove the duplicates
+jobs_to_exec = list(OrderedDict.fromkeys(get_jobs_to_exec(job)))
+
 
 # Merge all the steps from jobs to be executed
-def getJobSteps(result, job_name):
+def get_jobs_steps(result, job_name):
     job_steps = processed_config['jobs'][job_name]['steps']
     filtered_job_steps = list(filter(lambda step: list(step)[0] not in blacklisted_steps, job_steps))
     return result + filtered_job_steps
 
-merged_steps = reduce(getJobSteps, jobs_to_exec, [])
+
+merged_steps = reduce(get_jobs_steps, jobs_to_exec, [])
 
 # Create a new job, using the first job as a reference
 # This ensures configs like executor, environment, etc are maintained as it is from the first job
@@ -81,11 +104,11 @@ xformed_config['jobs'][xformed_job_name] = xformed_job
 
 # Create a transformed yaml
 with open(xformed_file, 'w+') as fstream:
-    yaml.dump(xformed_config,fstream)
+    yaml.dump(xformed_config, fstream)
 
 # Locally execute the newly created job
 # This job has all the steps(ordered and merged) from the mentioned job along with it's ancestors
 subprocess.check_call('circleci local execute -c {} --job {}'.format(xformed_file, xformed_job_name), shell=True)
 
 # Clean up, remove the processed and transformed yml files
-subprocess.check_call('rm {} {}'.format(processed_file, xformed_file), shell=True)
+subprocess.check_call('rm {} {} {}'.format(sw_cci_config_file, processed_file, xformed_file), shell=True)

32b792b7d02a3a416f709779ec51ccddb74df5c2
Prashant Sail <psail4444@gmail.com>
2020-06-17 19:10:30 +0530

    updates bzt yaml as per available metrics

--- tests/performance/metrics/__init__.py
+++ tests/performance/metrics/__init__.py
@@ -25,87 +25,62 @@ class ProcessType(Enum):
     ALL  = 3
 
 
-
-result = {
-    # 'sum_cpu_percent': None,
-    # 'avg_cpu_percent': None,
-    # 'min_cpu_percent': None,
-    # 'max_cpu_percent': None,
-    #
-    # 'sum_memory_percent': None,
-    # 'avg_memory_percent': None,
-    # 'min_memory_percent': None,
-    # 'max_memory_percent': None,
-    #
-    # 'sum_cpu_user_time': None,
-    # 'avg_cpu_user_time': None,
-    # 'min_cpu_user_time': None,
-    # 'max_cpu_user_time': None,
-    #
-    # 'sum_cpu_system_time': None,
-    # 'avg_cpu_system_time': None,
-    # 'min_cpu_system_time': None,
-    # 'max_cpu_system_time': None,
-    #
-    # 'sum_cpu_iowait_time': None,
-    # 'avg_cpu_iowait_time': None,
-    # 'min_cpu_iowait_time': None,
-    # 'max_cpu_iowait_time': None,
-    #
-    # 'sum_memory_rss': None,
-    # 'avg_memory_rss': None,
-    # 'min_memory_rss': None,
-    # 'max_memory_rss': None,
-    #
-    # 'sum_memory_vms': None,
-    # 'avg_memory_vms': None,
-    # 'min_memory_vms': None,
-    # 'max_memory_vms': None,
-    #
-    # 'sum_io_read_count': None,
-    # 'avg_io_read_count': None,
-    # 'min_io_read_count': None,
-    # 'max_io_read_count': None,
-    #
-    # 'sum_io_write_count': None,
-    # 'avg_io_write_count': None,
-    # 'min_io_write_count': None,
-    # 'max_io_write_count': None,
-    #
-    # 'sum_io_read_bytes': None,
-    # 'avg_io_read_bytes': None,
-    # 'min_io_read_bytes': None,
-    # 'max_io_read_bytes': None,
-    #
-    # 'sum_io_write_bytes': None,
-    # 'avg_io_write_bytes': None,
-    # 'min_io_write_bytes': None,
-    # 'max_io_write_bytes': None,
-    #
-    # 'sum_file_descriptors': None,
-    # 'avg_file_descriptors': None,
-    # 'min_file_descriptors': None,
-    # 'max_file_descriptors': None,
-    #
-    # 'sum_threads': None,
-    # 'avg_threads': None,
-    # 'min_threads': None,
-    # 'max_threads': None,
-    #
-    # 'sum_processes': None,
-    #
-    #
-    # 'system_disk_used': None,
-    # 'system_memory_used': None,
-    # 'system_read_count': None,
-    # 'system_read_bytes': None,
-    # 'system_write_bytes': None,
-
-    'total_workers': 0,
-    'frontend_memory_rss': None,
-    'sum_workers_memory_rss': None
+operators = {
+    'sum': sum,
+    'avg': mean,
+    'min': min,
+    'max': max
 }
-AVAILABLE_METRICS = list(result)
+
+process_metrics = {
+    'cpu_percent': lambda p: p.get('cpu_percent', 0),
+
+    'memory_percent': lambda p: p.get('memory_percent', 0),
+
+    'cpu_user_time': lambda p: getattr(p.get('cpu_times', {}), 'user', 0),
+    'cpu_system_time': lambda p: getattr(p.get('cpu_times', {}), 'system', 0),
+    'cpu_iowait_time': lambda p: getattr(p.get('cpu_times', {}), 'iowait', 0),
+
+    'memory_rss': lambda p: getattr(p.get('memory_info', {}), 'rss', 0),
+    'memory_vms': lambda p: getattr(p.get('memory_info', {}), 'vms', 0),
+
+    'io_read_count': lambda p: getattr(p.get('io_counters', {}), 'read_count', 0),
+    'io_write_count': lambda p: getattr(p.get('io_counters', {}), 'write_count', 0),
+    'io_read_bytes': lambda p: getattr(p.get('io_counters', {}), 'read_bytes', 0),
+    'io_write_bytes': lambda p: getattr(p.get('io_counters', {}), 'write_bytes', 0),
+
+    'file_descriptors': lambda p: p.get('num_fds', 0),
+
+    'threads': lambda p: p.get('num_threads', 0)
+}
+
+system_metrics = {
+    'total_processes': None,
+    'total_workers': None,
+    'system_disk_used': None,
+    'system_memory_percent': None,
+    'system_read_count': None,
+    'system_write_count': None,
+    'system_read_bytes': None,
+    'system_write_bytes': None,
+}
+
+AVAILABLE_METRICS = list(system_metrics)
+for metric in list(process_metrics):
+    for ptype in list(ProcessType):
+        if ptype == ProcessType.WORKER:
+            for op in list(operators):
+                type = 'workers'
+                AVAILABLE_METRICS.append('{}_{}_{}'.format(op,type,metric))
+        elif ptype == ProcessType.FRONTEND:
+            type = 'frontend'
+            AVAILABLE_METRICS.append('{}_{}'.format(type, metric))
+        else:
+            type = 'all'
+            for op in list(operators):
+                type = 'all'
+                AVAILABLE_METRICS.append('{}_{}_{}'.format(op,type,metric))
+
 
 def get_metrics(server_process, child_processes):
     """ Get Server processes specific metrics
@@ -115,78 +90,53 @@ def get_metrics(server_process, child_processes):
     # TODO - allow users to add new metrics easily
     # TODO - make sure available metric list is maintained
 
-    def update_metric(name, type, stats):
+    result = {}
+
+    def update_metric(metric_name, type, stats):
+        stats = stats if stats else [0]
+
         if type == ProcessType.WORKER:
-            result['total_workers'] = len(stats) - 1
-            type = "workers"
+            type = 'workers'
         elif type == ProcessType.FRONTEND:
-            result['frontend_' + name] = stats[0]
+            result['frontend_' + metric_name] = stats[0]
             return
         else:
-            type="all"
+            type='all'
 
-        result['sum_' + type+'_' + name] = sum(stats)
-        result['avg_' + type+'_' + name] = mean(stats)
-        result['min_' + type+'_' + name] = min(stats)
-        result['max_' + type+'_' + name] = max(stats)
+        for op_name in operators:
+            result['{}_{}_{}'.format(op_name, type, metric_name)] = operators[op_name](stats)
 
+    # as_dict() gets all stats in one shot
     processes_stats = []
     processes_stats.append({'type': ProcessType.FRONTEND, 'stats': server_process.as_dict()})
     for process in child_processes:
-        # Get all stats in one shots
         processes_stats.append({'type': ProcessType.WORKER, 'stats' : process.as_dict()})
 
     ### PROCESS METRICS ###
-
-    metrics = {
-        'cpu_percent': lambda p: p.get('cpu_percent', 0),
-
-        'memory_percent': lambda p: p.get('memory_percent', 0),
-
-        'cpu_user_time': lambda p: getattr(p.get('cpu_times', {}), 'user', 0),
-        'cpu_system_time': lambda p: getattr(p.get('cpu_times', {}), 'system', 0),
-        'cpu_iowait_time': lambda p: getattr(p.get('cpu_times', {}), 'iowait', 0),
-
-        'memory_rss': lambda p: getattr(p.get('memory_info', {}), 'rss', 0),
-        'memory_vms': lambda p: getattr(p.get('memory_info', {}), 'vms', 0),
-
-        'io_read_count': lambda p: getattr(p.get('io_counters', {}), 'read_count', 0),
-        'io_write_count': lambda p: getattr(p.get('io_counters', {}), 'write_count', 0),
-        'io_read_bytes': lambda p: getattr(p.get('io_counters', {}), 'read_bytes', 0),
-        'io_write_bytes': lambda p: getattr(p.get('io_counters', {}), 'write_bytes', 0),
-
-        'file_descriptors': lambda p: p.get('num_fds', 0),
-
-        'threads': lambda p: p.get('num_threads', 0)
-    }
-
-    for k in metrics:
+    for k in process_metrics:
 
         worker_stats = list(map(lambda x: x['stats'], filter(lambda x: x['type'] == ProcessType.WORKER, processes_stats)))
         all_stats = list(map(lambda x: x['stats'], processes_stats))
         server_stats = list(map(lambda x: x['stats'], filter(lambda x: x['type'] == ProcessType.FRONTEND, processes_stats)))
 
-        if len(worker_stats): update_metric(k, ProcessType.WORKER, list(map(metrics[k], worker_stats)))
-        if len(all_stats): update_metric(k, ProcessType.ALL, list(map(metrics[k], all_stats)))
-        if len(server_stats): update_metric(k, ProcessType.FRONTEND, list(map(metrics[k], server_stats)))
+        update_metric(k, ProcessType.WORKER, list(map(process_metrics[k], worker_stats)))
+        update_metric(k, ProcessType.ALL, list(map(process_metrics[k], all_stats)))
+        update_metric(k, ProcessType.FRONTEND, list(map(process_metrics[k], server_stats)))
 
 
     # Total processes
-    result['sum_processes'] = len(processes_stats)
+    result['total_processes'] = len([server_process] + child_processes)
+    result['total_workers'] = len(child_processes) - 1
 
     ### SYSTEM METRICS ###
-    result["system_disk_used"] = psutil.disk_usage('/').used
+    result['system_disk_used'] = psutil.disk_usage('/').used
 
-    result["system_memory_percent"] = psutil.virtual_memory().percent
+    result['system_memory_percent'] = psutil.virtual_memory().percent
 
     system_disk_io_counters = psutil.disk_io_counters()
-    result["system_read_count"] = system_disk_io_counters.read_count
-    result["system_write_count"] = system_disk_io_counters.write_count
-    result["system_read_bytes"] = system_disk_io_counters.read_bytes
-    result["system_write_bytes"] = system_disk_io_counters.write_bytes
-
-    print('total_workers          :', result["total_workers"])
-    print('frontend_memory_rss    :', result["frontend_memory_rss"])
-    print('sum_workers_memory_rss :', result["sum_workers_memory_rss"])
+    result['system_read_count'] = system_disk_io_counters.read_count
+    result['system_write_count'] = system_disk_io_counters.write_count
+    result['system_read_bytes'] = system_disk_io_counters.read_bytes
+    result['system_write_bytes'] = system_disk_io_counters.write_bytes
 
     return result

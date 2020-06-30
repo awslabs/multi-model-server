a320bcde4c08fcc374c5dcad534626394bb598b6
Prashant Sail <psail4444@gmail.com>
2020-06-30 17:31:24 +0530

    all performance scenarios,partially roll back changes to config.yml

--- .circleci/scripts/linux_test_perf_regression.sh
+++ .circleci/scripts/linux_test_perf_regression.sh
@@ -4,16 +4,13 @@ ARTIFACTS_DIR='run_artifacts'
 RESULT_DIR=$ARTIFACTS_DIR'/report/performance/'
 JMETER_PATH='/opt/apache-jmeter-5.3/bin/jmeter'
 
-# Start MMS server
-#multi-model-server --start >> mms.log 2>&1
-#sleep 10
-
 cd tests/performance
 
 # Only on a python 2 environment -
 PY_MAJOR_VER=$(python -c 'import sys; major = sys.version_info.major; print(major);')
 if [ $PY_MAJOR_VER -eq 2 ]; then
   # Hack to use python 3.6.5 for bzt installation and execution
+  # While MMS continues to use system python which is at 2.7.x
   export PATH="/root/.pyenv/bin:/root/.pyenv/shims:$PATH"
   pyenv local 3.6.5 system
 fi
@@ -23,11 +20,8 @@ pip install -r requirements.txt
 pip install bzt
 
 # Execute performance test suite and store exit code
-./run_performance_suite.py -j $JMETER_PATH -e xlarge --no-compare-local -p health_check
+./run_performance_suite.py -j $JMETER_PATH -e xlarge --no-compare-local
 EXIT_CODE=$?
-
-# Stop server
-#multi-model-server --stop >> mms.log 2>&1
 
 # Collect and store test results in result directory to be picked up by CircleCI
 mkdir -p $RESULT_DIR
@@ -35,4 +29,3 @@ cp $ARTIFACTS_DIR/**/performance_results.xml $RESULT_DIR
 
 # Exit with the same error code as that of test execution
 exit EXIT_CODE
-

6baf41d6905298836ec1c46ddce77bb10a0557a3
Shivam Shriwas <shivamshriwas21@gmail.com>
2020-07-31 00:53:32 +0530

    updated circle-ci scripts

--- .circleci/scripts/linux_test_modelarchiver.sh
+++ .circleci/scripts/linux_test_modelarchiver.sh
@@ -4,9 +4,11 @@ cd model-archiver/
 
 # Lint test
 pylint -rn --rcfile=./model_archiver/tests/pylintrc model_archiver/.
+PY_LINT_EXIT_CODE=$?
 
 # Execute python unit tests
 python -m pytest --cov-report html:results_units --cov=./ model_archiver/tests/unit_tests
+PY_UNITS_EXIT_CODE=$?
 
 
 # Install model archiver module
@@ -14,4 +16,10 @@ pip install .
 
 # Execute integration tests
 python -m pytest model_archiver/tests/integ_tests
+PY_INTEG_EXIT_CODE=$?
 # ToDo - Report for Integration tests ?
+
+# If any one of the steps fail, exit with error
+if [ "$PY_LINT_EXIT_CODE" -ne 0 ] || [ "$PY_UNITS_EXIT_CODE" -ne 0 ] || [ "$MODL_ARCHVR_EXIT_CODE" -ne 0 ] || [ "$PY_INTEG_EXIT_CODE" -ne 0 ]
+then exit 1
+fi

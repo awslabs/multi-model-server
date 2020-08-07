002091a80531497beda4fd97f6da5ad6c7e3eca1
Shivam Shriwas <shivamshriwas21@gmail.com>
2020-08-07 16:22:10 +0530

    added python shebang

--- tests/performance/run_performance_suite.py
+++ tests/performance/run_performance_suite.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 # Copyright 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # Licensed under the Apache License, Version 2.0 (the "License").
 # You may not use this file except in compliance with the License.

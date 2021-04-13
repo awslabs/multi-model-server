290b1649e1075a5750add05a7c476ca44c86824a
Ubuntu <ubuntu@ip-172-31-54-208.us-west-2.compute.internal>
2021-04-13 18:36:28 +0000

    Fix build error

--- mms/service.py
+++ mms/service.py
@@ -136,7 +136,7 @@ class PredictionException(Exception):
         self.message = message
         self.error_code = error_code
     def __str__(self):
-        return f"self.message : self.error_code"
+        return f"{self.message} : {self.error_code}"
 
 
 def emit_metrics(metrics):
